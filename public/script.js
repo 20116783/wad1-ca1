e05055d317c96c492ca779c7a9fd7f43fcfa49cd
Nikita Zdunek <233318125+20116783@users.noreply.github.com>
2026-03-04 16:43:39 +0000

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,12 +1,9 @@
-/* This script runs on page load */
 $(document).ready(function() {
     console.log("Console Archive Ready!");
 
-    // If we are on the welcome page, show a welcome toast/popup
     if(window.location.pathname === '/') {
         $('.ui.header').transition('pulse');
 
-        // Simple JS Output requirement
         setTimeout(() => {
             alert("Welcome to the Console Archive! Click 'View Collection' to start.");
         }, 500);
